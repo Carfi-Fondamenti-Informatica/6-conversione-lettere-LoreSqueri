c9f0664f77091401300253bf7887569a6c7a1768
LoreSqueri <91483152+LoreSqueri@users.noreply.github.com>
2021-11-29 13:31:52 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,10 @@
+//
+// Created by squer on 29/11/2021.
+//
 
+#ifndef ESERCITAZIONE_6_LIBRERIA_H
+#define ESERCITAZIONE_6_LIBRERIA_H
+
+#endif //ESERCITAZIONE_6_LIBRERIA_H
+
+bool conversione (char&);
